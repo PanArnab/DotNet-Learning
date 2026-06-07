ef7b194d2822fb1097c6c9b60e3ef16da87043d9
PanArnab <160526916+PanArnab@users.noreply.github.com>
2026-06-08 02:07:25 +0530

    ap

---
+++ Shared.Contracts/Demo/Models/IndustrySector.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Contracts.Demo.Models
+{
+    public class IndustrySector
+    {
+        [Column("IS_Id")]
+        public int IndustrySectorId { get; set; }
+
+        [Column("Comp_Id")]
+        public int CompanyId { get; set; }
+
+        [Column("IS_Name")]
+        public string Name { get; set; }
+
+        [Column("IS_Code")]
+        public string Code { get; set; }
+
+        [Column("IS_IsActive")]
+        public bool IsActive { get; set; } = true;
+
+        [Column("IS_IsDeleted")]
+        public bool IsDeleted { get; set; } = false;
+    }
+}
